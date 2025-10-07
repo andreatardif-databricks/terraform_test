7ded6995f00ffaf408bc1500bf20b579d772bcf2
Andrea Tardif <andrea.tardif@databricks.com>
2025-10-07 11:57:07 -0400

    Use Google Service Account authentication for Databricks provider - proper OAuth token method

--- providers.tf
+++ providers.tf
@@ -29,15 +29,13 @@ provider "google" {
 // initialize provider in "accounts" mode to provision new workspace
 
 provider "databricks" {
-  alias         = "accounts"
-  host          = "https://accounts.gcp.databricks.com"
-  account_id    = var.databricks_account_id
-  client_id     = var.databricks_sp_client_id
-  client_secret = var.databricks_sp_client_secret
+  alias                       = "accounts"
+  host                        = "https://accounts.gcp.databricks.com"
+  account_id                  = var.databricks_account_id
+  google_service_account      = var.databricks_google_service_account
 }
 
 provider "databricks" {
-  host          = databricks_mws_workspaces.databricks_workspace.workspace_url
-  client_id     = var.databricks_sp_client_id
-  client_secret = var.databricks_sp_client_secret
+  host                   = databricks_mws_workspaces.databricks_workspace.workspace_url
+  google_service_account = var.databricks_google_service_account
 }
