f95bbdfed6dd956ee7af6ff9e14c5b759fd42a77
Andrea Tardif <andrea.tardif@databricks.com>
2025-10-08 11:00:22 -0400

    Updates

--- workspace.tf
+++ workspace.tf
@@ -7,15 +7,22 @@ variable "gke_pod_subnet" {}
 variable "gke_service_subnet" {}
 variable "gke_master_ip_range" {}
 
-# Use existing Databricks network (service principal lacks network creation permission)
-locals {
-  existing_network_id = "6a1a267a-a1a3-4a4f-9de7-a1be326cc2f6"
+resource "databricks_mws_networks" "this" {
+  provider     = databricks.accounts
+  account_id   = var.databricks_account_id
+  network_name = "${var.google_project_name}-nw-${random_string.suffix.result}"
+  gcp_network_info {
+    network_project_id    = var.google_project_name
+    vpc_id                = var.google_vpc_id
+    subnet_id             = var.gke_node_subnet
+    subnet_region         = var.google_region
+  }
 }
 
 resource "databricks_mws_workspaces" "this" {
   provider       = databricks.accounts
   account_id     = var.databricks_account_id
-  workspace_name = "tf-demo-test-${random_string.suffix.result}"
+  workspace_name = var.databricks_workspace_name
   location       = var.google_region
   cloud_resource_container {
     gcp {
@@ -23,7 +30,7 @@ resource "databricks_mws_workspaces" "this" {
     }
   }
 
-  network_id = local.existing_network_id
+  network_id = databricks_mws_networks.this.network_id
 }
 
 output "databricks_host" {
